389e69c6c4e95c444fdc585aa46f41dce29f7340
LisaDobruk <liskam8525gmail.com>
2022-11-07 22:47:58 +0100

    feat: add code for 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,27 @@
 
+const fs = require('fs/promises');
+const path = require('path');
+const src = path.join(__dirname, 'files');
+const dest = path.join(__dirname, 'files-copy');
+
+const copyFolder = async() => {
+   try{ 
+    await fs.rm(dest, {recursive: true, force: true}, (err)=>{
+        if(err){
+            console.error(err);
+        }
+    });
+
+    await fs.mkdir(dest, {recursive: true});
+    let files = await fs.readdir(src);
+    
+
+     for(let file of files){
+        let srcPath = path.join(src, file);
+        let destPath = path.join(dest, file);
+
+        await fs.copyFile(srcPath, destPath);
+     }
+    }catch(err){console.log(err)}
+}
+copyFolder();
